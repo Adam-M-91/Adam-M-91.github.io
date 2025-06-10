e4de8e934601950e0ceadb145360441d08aac00b
Adam-M-91 <amacanowicz@yahoo.ca>
2025-06-10 02:37:48 +0000

    git commit -m "message"

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,4 +182,19 @@
 
 			});
 
+			function copyURI(evt) {
+
+		async function copyContent() {
+  try {
+    await navigator.clipboard.writeText('info@adam-macanowicz.com');
+    console.log('Content copied to clipboard');
+    /* Resolved - text copied to clipboard successfully */
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+    /* Rejected - text failed to copy to the clipboard */
+  }
+}		
+   
+}
+
 })(jQuery);
